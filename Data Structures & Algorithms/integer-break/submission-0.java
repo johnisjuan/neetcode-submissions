a75a6ea994fc078345061f62463cd24b504d2c1b
John John <johnj4.2005@gmail.com>
2026-09-01 15:46:43 +0530

    Add: integer-break - submission-0

---
+++ Data Structures & Algorithms/integer-break/submission-0.java
@@ -0,0 +1,24 @@
+public class Solution {
+    private Map<Integer, Integer> dp;
+
+    public int integerBreak(int n) {
+        dp = new HashMap<>();
+        dp.put(1, 1);
+        return dfs(n, n);
+    }
+
+    private int dfs(int num, int n) {
+        if (dp.containsKey(num)) {
+            return dp.get(num);
+        }
+
+        int res = (num == n) ? 0 : num;
+        for (int i = 1; i < num; i++) {
+            int val = dfs(i, n) * dfs(num - i, n);
+            res = Math.max(res, val);
+        }
+
+        dp.put(num, res);
+        return res;
+    }
+}
